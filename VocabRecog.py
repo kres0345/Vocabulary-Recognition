950b6da2bfe17d7f5995b406b6a98cf30c976182
kres0345 <kressckerl@gmail.com>
2018-06-24 19:47:50 +0200

    Updated

--- VocabRecog.py
+++ VocabRecog.py
@@ -6,9 +6,13 @@ from os.path import join
 comparisonFolderPath = 'Comparison data'
 inputFileName = 'input.txt'
 
-global ComparisonData, CompFileNamesGlob;
+
+inputData = sub("[^a-zA-Z ]+", "", open(inputFileName,'r').read().replace("\n"," "))
+inputData = inputData.split(" ")
+global ComparisonData, CompFileNamesGlob, CompNegScores;
 ComparisonData = []
 CompFileNamesGlob = []
+CompNegScores = []
 
 def LoadComparisonData():
     global ComparisonData, CompFileNamesGlob;
@@ -20,8 +24,45 @@ def LoadComparisonData():
 
 def CompareData():
     LoadComparisonData()
+    global ComparisonData, CompNegScores, CompFileNamesGlob;
+    i=0
+    while(i<len(CompFileNamesGlob)):
+        CompNegScores.append(0)
+        f=0
+        while(f<len(inputData)):
+            if not(inputData[f] in ComparisonData[i]):
+                CompNegScores[i] += 1
+            f+=1
+        i+=1
+
     #Insert complex cool code here :D
+'''
+def percent(num1, num2):
+    num1 = float(num1)
+    num2 = float(num2)
+    try:
+        result = (num1 / num2 * 100) #num1 / num2 * 100
+    except ZeroDivisionError:
+        result = 100
+    percentage = '{0:.2f}'.format(result)
+    return percentage'''
 
-
+def percent(a, b):
+    try:
+        return round(a / b * 100, 2)
+    except ZeroDivisionError:
+        return round(100, 2)
 
 CompareData()
+print('''
+---------------------------
+--- Input file: {0}
+--- Comparison files: {1}
+---------------------------
+'''.format(inputFileName, str(CompFileNamesGlob)))
+i=0
+while(i<len(CompFileNamesGlob)):
+    print("-{0} unlikelihood: {1}, Words not in vocab: {2} / {3}".format(CompFileNamesGlob[i], str(percent(CompNegScores[i],len(inputData)))+'%',CompNegScores[i], len(inputData)))
+    i+=1
+#print(CompFileNamesGlob)
+#print(CompNegScores)
